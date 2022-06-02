faedc294423a903635dd82f065925b9b629d057c
Francesco Patrizio Zampetti <zampetti1@hotmail.com>
2022-06-02 15:36:52 +0200

    aggiunti endpoints.

--- src/main/java/com/blackphoenixproductions/forumfrontend/client/ForumClient.java
+++ src/main/java/com/blackphoenixproductions/forumfrontend/client/ForumClient.java
@@ -1,15 +1,20 @@
 package com.blackphoenixproductions.forumfrontend.client;
 
 import com.blackphoenixproductions.forumfrontend.dto.Filter;
+import com.blackphoenixproductions.forumfrontend.dto.NotificationDTO;
+import com.blackphoenixproductions.forumfrontend.dto.post.InsertPostDTO;
+import com.blackphoenixproductions.forumfrontend.dto.post.PostDTO;
+import com.blackphoenixproductions.forumfrontend.dto.topic.EditTopicDTO;
+import com.blackphoenixproductions.forumfrontend.dto.topic.InsertTopicDTO;
+import com.blackphoenixproductions.forumfrontend.dto.topic.TopicDTO;
 import com.blackphoenixproductions.forumfrontend.dto.topic.VTopicDTO;
 import com.blackphoenixproductions.forumfrontend.dto.user.SimpleUserDTO;
-import feign.Headers;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 
 
 @FeignClient(value = "forum-client", path = "/api", url = "${backend.path}")
@@ -17,22 +22,50 @@ public interface ForumClient {
 
     String AUTH_TOKEN = "Authorization";
 
-
     @GetMapping(value = "/getTotalUsers")
-    ResponseEntity<Long> getTotalUsers ();
+    ResponseEntity<Long> getTotalUsers();
 
     @GetMapping(value = "/getTotalTopics")
-    ResponseEntity<Long> getTotalTopics ();
+    ResponseEntity<Long> getTotalTopics();
 
     @GetMapping(value = "/getTotalPosts")
-    ResponseEntity<Long> getTotalPosts ();
+    ResponseEntity<Long> getTotalPosts();
 
     @PostMapping(value = "/findFilteredTopicsByPage")
-    ResponseEntity<PagedModel<EntityModel<VTopicDTO>>> findFilteredTopicsByPage (@RequestParam int page, @RequestParam int size,
+    ResponseEntity<PagedModel<EntityModel<VTopicDTO>>> findFilteredTopicsByPage(@RequestParam int page, @RequestParam int size,
                                                                                  @RequestBody(required = false) Filter filter);
+    @PostMapping(value = "/findPostsByPage")
+    ResponseEntity<PagedModel<EntityModel<PostDTO>>> findPostsByPage(@RequestParam Long topicId, @RequestParam int page, @RequestParam int size);
+
+    @PostMapping(value = "createPost")
+    ResponseEntity<EntityModel<PostDTO>> createPost(@RequestHeader(AUTH_TOKEN) String bearerToken, @RequestBody InsertPostDTO postDTO);
+
+    @GetMapping(value = "/findTopic")
+    ResponseEntity<EntityModel<TopicDTO>> findTopic(@RequestParam Long id);
+
+    @PostMapping(value = "createTopic")
+    ResponseEntity<EntityModel<TopicDTO>> createTopic(@RequestHeader(AUTH_TOKEN) String bearerToken, @RequestBody InsertTopicDTO insertTopicDTO);
+
+    @PostMapping(value = "editTopic")
+    ResponseEntity<EntityModel<TopicDTO>> editTopic(@RequestHeader(AUTH_TOKEN) String bearerToken, @RequestBody EditTopicDTO topicDTO);
+
     @GetMapping (value = "/retriveUser")
-    ResponseEntity<EntityModel<SimpleUserDTO>> retriveUser (@RequestHeader(AUTH_TOKEN) String bearerToken);
-//    ResponseEntity<EntityModel<SimpleUserDTO>> retriveUser ();
+    ResponseEntity<EntityModel<SimpleUserDTO>> retriveUser(@RequestHeader(AUTH_TOKEN) String bearerToken);
+
+    @PostMapping (value = "/changeUserUsername")
+    ResponseEntity<EntityModel<SimpleUserDTO>> changeUserUsername(@RequestHeader(AUTH_TOKEN) String bearerToken, @RequestParam String newUsername);
+
+    @GetMapping(value = "/getBuildVersionBackEnd")
+    ResponseEntity<String> getBuildVersionBackEnd();
+
+    @GetMapping(value = "getUserNotificationList")
+    ResponseEntity<CollectionModel<NotificationDTO>> getUserNotificationList(@RequestHeader(AUTH_TOKEN) String bearerToken);
+
+    @GetMapping(value = "getUserNotificationStatus")
+    ResponseEntity<Boolean> getUserNotificationStatus(@RequestHeader(AUTH_TOKEN) String bearerToken);
+
+    @GetMapping(value = "setReadedNotificationStatus")
+    ResponseEntity<String> setReadedNotificationStatus(@RequestHeader(AUTH_TOKEN) String bearerToken);
 
 
 }
