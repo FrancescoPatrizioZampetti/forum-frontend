216b7069da55698f78e173c15345c135bc7da283
Francesco Patrizio Zampetti <zampetti1@hotmail.com>
2022-06-06 22:53:22 +0200

    thymeleaf-extras-springsecurity5

--- src/main/java/com/blackphoenixproductions/forumfrontend/dto/post/PostDTO.java
+++ src/main/java/com/blackphoenixproductions/forumfrontend/dto/post/PostDTO.java
@@ -1,11 +1,13 @@
 package com.blackphoenixproductions.forumfrontend.dto.post;
 
+import com.blackphoenixproductions.forumfrontend.dto.user.UserDTO;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Getter
 @Setter
@@ -18,5 +20,22 @@ public class PostDTO {
     private LocalDateTime createDate;
     private LocalDateTime deleteDate;
     private LocalDateTime editDate;
+    private UserDTO user;
+    private String formattedCreateDate;
+    private String formattedEditDate;
+
+    public String getFormattedCreateDate() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("'Inviato il' dd/MM/yy 'alle' HH:mm");
+        return formatter.format(createDate);
+    }
+
+    public String getFormattedEditDate() {
+        String formattedDate = null;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("'Ultima modifica il' dd/MM/yy 'alle' HH:mm");
+        if(editDate != null) {
+            formattedDate = formatter.format(editDate);
+        }
+        return formattedDate;
+    }
 
 }
