145b50e4b17937dbe880ebece6cdfa1f83c526fc
Francesco Patrizio Zampetti <zampetti1@hotmail.com>
2022-06-07 23:35:08 +0200

    refactor.

--- src/main/java/com/blackphoenixproductions/forumfrontend/exception/MyErrorController.java
+++ src/main/java/com/blackphoenixproductions/forumfrontend/exception/MyErrorController.java
@@ -3,7 +3,6 @@ package com.blackphoenixproductions.forumfrontend.exception;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 
