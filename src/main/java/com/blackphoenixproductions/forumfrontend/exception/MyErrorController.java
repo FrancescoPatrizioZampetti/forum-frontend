8076385ff548abeb26998b72d634ca4d9a0e530b
Francesco Patrizio Zampetti <zampetti1@hotmail.com>
2022-06-10 12:31:28 +0200

    error page.

--- src/main/java/com/blackphoenixproductions/forumfrontend/exception/MyErrorController.java
+++ src/main/java/com/blackphoenixproductions/forumfrontend/exception/MyErrorController.java
@@ -1,18 +1,29 @@
 package com.blackphoenixproductions.forumfrontend.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class MyErrorController implements ErrorController {
 
+    private static final Logger logger = LoggerFactory.getLogger(MyErrorController.class);
+    private final String DEFAULT_ERROR_ATTRIBUTE = "org.springframework.boot.web.servlet.error.DefaultErrorAttributes.ERROR";
 
-    @GetMapping(value="/error")
-    public String genericError(HttpServletRequest httpServletRequest){
-        return "redirect:/login";
+
+    @GetMapping(value = "/error")
+    public String genericError(Model model, HttpServletRequest httpServletRequest) {
+        Object status = httpServletRequest.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        Object errorAttribute = httpServletRequest.getAttribute(DEFAULT_ERROR_ATTRIBUTE);
+        logger.error("Errore : {} , statusCode : {}", errorAttribute, status);
+        model.addAttribute("status", status);
+        return "error";
     }
 
 }
