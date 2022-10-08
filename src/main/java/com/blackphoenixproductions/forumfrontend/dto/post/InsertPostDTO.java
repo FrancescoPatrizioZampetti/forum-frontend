242e315bd90b481b427f1a2ea8d15be02ad8ea9c
Francesco Patrizio Zampetti <zampetti1@hotmail.com>
2022-10-08 19:01:15 +0200

    fix.

--- src/main/java/com/blackphoenixproductions/forumfrontend/dto/post/InsertPostDTO.java
+++ src/main/java/com/blackphoenixproductions/forumfrontend/dto/post/InsertPostDTO.java
@@ -4,16 +4,13 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Size;
+
 
 @Getter
 @Setter
 public class InsertPostDTO {
     @NotBlank(message = "Il messaggio del post non può essere null/vuoto")
     private String message;
-    @NotEmpty(message = "Il messaggio del post non puo' essere null/vuoto")
-    @Size(max = 20000, message = "Il messaggio del post ha superato il massimo di 20000 caratteri")
     private Long topicId;
 
     public InsertPostDTO(String message, Long topicId) {
